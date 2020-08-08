c0dbcd4ffb97ac01051c5921bd1fa230cc3af566
AntiqueR <antique.pendulum@gmail.com>
2020-08-08 19:19:14 +0900

    add file manage(read and write)

---
+++ OilView/Models/FileData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OilView.Models
+{
+    public class FileData
+    {
+        public DateTime Day { get; set; }
+
+        public string ToDoJob { get; set; }
+
+        public string DoneJob { get; set; }
+
+        public FileData(DateTime day, string todoJob, string doneJob)
+        {
+            Day = day;
+            ToDoJob = todoJob;
+            DoneJob = doneJob;
+        }
+    }
+}
---
+++ OilView/Models/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utf8Json;
+
+namespace OilView.Models
+{
+    public class FileManager : IFileRepository
+    {
+        public async Task<FileData> ReadAsync(string path)
+        {
+            if (!File.Exists(path)) throw new FileNotFoundException("ファイルが見つかりませんでした。");
+            using (var stream = File.Open(path, FileMode.Open))
+            {
+                return await JsonSerializer.DeserializeAsync<FileData>(stream);
+            }
+        }
+
+        public async Task WriteAsync(string path, FileData fileData)
+        {
+            using (var stream = File.Open(path, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(stream, fileData);
+            }
+        }
+    }
+}
